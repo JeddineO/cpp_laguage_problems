a205bdd2e79109d560496790770ca309e8f778dc
jeo <outgougajalaldine@gmail.com>
2023-11-24 15:20:37 +0100

    init

--- 8-Forme/main.cpp
+++ 8-Forme/main.cpp
@@ -1,17 +1,22 @@
- //#include "Carre.cpp"
- //#include "Rectangle.cpp"
- #include "Cercle.cpp"
+#include "Carre.cpp"
+#include "Rectangle.cpp"
+#include "Cercle.cpp"
 #include "Cube.cpp"
 
 int main()
 {
     Forme *list[6];
 
-    // list[0] = new Rectangle(5, 8);
-    // list[0] = new Cercle(5);
-    // list[0] = new Carre(6);
-    list[0] = new Cube(6);
+    list[0] = new Rectangle(5, 8);
+    list[1] = new Cercle(5);
+    list[2] = new Carre(6);
+    list[3] = new Cube(6);
 
-    list[0]->afficher();
+    for (int i = 0; i < 4; i++)
+    {
+        list[i]->afficher();
+        cout << "Surface :" << list[i]->surface() << endl;
+    }
+
     return 0;
 }
